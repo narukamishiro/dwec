8e03234154ceff4b9823ed5c712b84b56f35da89
isidro lucena santiago <darksirius91@gmail.com>
2017-10-26 13:23:07 +0200

    ejecicios redaccio 1 tema 4 (26/10/17)

--- tema 4/redaccion 1/fnEvent.js
+++ tema 4/redaccion 1/fnEvent.js
@@ -5,7 +5,40 @@ function gps(event){
 	var mnx=document.getElementById("px");
 	var mny=document.getElementById("py");
 	mnx.innerHTML="posicion x: "+event.screenX;
-	mny.innerHTML="posicion y: "+event.screenY;
+	mny.text="posicion y: "+event.screenY;
+}
+function createtb(){
+	var cod="<table id='tb' border='1'>";
+	var n=1;
+	for(x=1;x<=100;x++){
+		cod=cod+"<tr>";
+		for(y=1;y<=100;y++){
+			cod=cod+"<td id="+n+" ></td>";
+			n++;
+		}
+		cod=cod+"</tr>";
+	}
+	cod=cod+"</table>";
+	return cod;
+}
+function pintv1(event){
+	if(event.shiftKey){
+		event.style.backgroundColor='#FF0253';
+	}
+	if(event.ctrlKey){
+		event.style.backgroundColor='#FF1061';
+	}
+	if(event.wich==99){
+		event.style.backgroundColor='#FFFFFF';
+	}
+	if(event.wich==97){
+		for(z=1;z<=100;z++){
+			for(p=1;p<=100;p++){
+				n=100*(z-1)+p;
+				document.getElementById(n).style.backgroundColor='#FFFFFF';
+			}
+		}
+	}
 }
 
 //ejercicio 1
@@ -13,5 +46,13 @@ function gps(event){
 window.onclick=click;
 */
 // ejercicio 2
-window.onload;
-document.getElementById("gps").addEventListener("onmousemove",gps);
+//document.addEventListener("onmousemove",gps);
+//window.onload;
+//ejercicio 4 & 5
+/*document.body.innerHTML=createtb();
+	for(z=1;z<=100;z++){
+	for(p=1;p<=100;p++){
+		n=100*(z-1)+p;
+		document.getElementById(n).addEventListener("onmousemove",pintv1);
+	}
+}*/
